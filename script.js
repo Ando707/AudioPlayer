f06aaa1ea69cd15a99ab57c62c9a80bf47dbc08d
Ando707 <a.khachatryan.2001a@gmail.com>
2022-08-31 14:55:11 +0400

    generating play list menu

--- script.js
+++ script.js
@@ -2,52 +2,57 @@ let dataPlayList = [
     {
         key: 'KeyZ',
         src: './audio/Rihanna - Diamonds.mp3',
-        btn: 'Z'
+        btn: 'Z - Rihanna - Diamonds'
     },
     {
         key: 'KeyX',
         src: './audio/Aram Asatrayn - Patilner.mp3',
-        btn: 'X'
+        btn: 'X - Aram Asatrayn - Patilner'
     },
     {
         key: 'KeyC',
         src: './audio/hayesDu.mp3',
-        btn: 'C'
+        btn: 'C - Aram Asatrayn - hayesDu'
     },
     {
         key: 'KeyV',
         src: "./audio/INSTASAMKA_-_moneyken-love.mp3",
-        btn: 'V'
+        btn: 'V - INSTASAMKA_-_moneyken-love'
     },
     {
         key: 'KeyB',
         src: "./audio/miyagi_-_marmalade.mp3",
-        btn: 'B'
+        btn: 'B - miyagi_-_marmalade'
     },
     {
         key: 'KeyN',
         src: "./audio/egor-krid_-_we-gotta-get-love.mp3",
-        btn: 'N'
+        btn: 'N - egor-krid_-_we-gotta-get-love'
     },
 ]
 const playList = document.querySelector('.play-list');
 
 class Audio {
-    constructor( key, src ){
+    constructor( key, src, btn ){
         this.src = src
-        this.key = key;
+        this.key = key
+        this.btn = btn
     }
     render(){
        return  `
+            
             <audio controls id="${this.key}">
                 <source src="${this.src}"  type="audio/mpeg">
-            </audio> 
+            </audio>
+            <span class="fs-3 px-4 text-success">
+                ${this.btn}
+            </span> 
        `
     }
 }
 
 function createAudio( arg ){
-    playList.innerHTML = new Audio( arg.key, arg.src ).render();
+    playList.innerHTML = new Audio( arg.key, arg.src, arg.btn ).render();
     let audio = document.getElementById( arg.key );
     audio.play();
 }
@@ -86,3 +91,16 @@ document.addEventListener('keydown', (e) => {
 
 
 
+const playListMenu = document.querySelector('.play-list-menu')
+
+function createMenuItem( a ){
+    return `
+        <li class="nav-item">
+            <span class="text-success fs-1 fw-semibold px-2"> ${ a.btn } </span>
+        </li>
+    `
+}
+
+for( let obj of dataPlayList ){
+    playListMenu.insertAdjacentHTML('beforeend', createMenuItem( obj ))
+}
