7639507b512c3221521f7f8a51f39a7ff1a17d03
Ando707 <a.khachatryan.2001a@gmail.com>
2022-09-15 11:10:09 +0400

    Audio Data

---
+++ Data.js
@@ -0,0 +1,163 @@
+
+export let data = [
+  {
+      src: './audio/Aram Asatrayn - Patilner.mp3',
+      name: 'Aram Asatrayn - Patilner',
+      code: 'KeyA',
+      type: 'classic',
+  },
+  {
+      src: './audio/miyagi-scriptonit.mp3',
+      name: 'Miyagi - Scriptonit',
+      code: 'KeyB',
+      type: 'rock',
+  },
+  {
+      src: './audio/MATRANG-MEDUZA.mp3',
+      name: 'Matrang - Meduzda',
+      code: 'KeyC',
+      type: 'pop',
+  },
+  {
+      src: './audio/Aram Mp3, Forsh-Requiem.mp3',
+      name: 'Aram Mp3 & Forsh - Requiem',
+      code: 'KeyD',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Aram Asatryan-Chanaparh.mp3',
+      name: 'Aram Asatrayn - Chanaparh',
+      code: 'KeyE',
+      type: 'classic',
+  },
+  {
+      src: './audio/Miyagi &amp; Andy Panda-Minor.mp3',
+      name: 'Miyagi & Andy Panda - Minor',
+      code: 'KeyF',
+      type: 'rock',
+  },
+  {
+      src: './audio/Dlya_Dushi😍-Muzika Dlya Dushi.mp3',
+      name: 'Dlya Dushi - Muzika Dlya Dushi',
+      code: 'KeyG',
+      type: 'pop',
+  },
+  {
+      src: './audio/Arkadi Dumikyan-Sarerov dzorerov.mp3',
+      name: 'Arkadi Dumikyan - Sarerov dzorerov',
+      code: 'KeyH',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Artash Asatryan-Gavat.mp3',
+      name: 'Artash Asatryan - Gavat',
+      code: 'KeyI',
+      type: 'classic',
+  },
+  {
+      src: './audio/miyagi-andy-panda_-_patron.mp3',
+      name: 'Miyagi & Andy Panda - Patron',
+      code: 'KeyJ',
+      type: 'rock',
+  },
+  {
+      src: './audio/Misha Xramovi-Chayare.mp3',
+      name: 'Misha Xramovi - Chayare',
+      code: 'KeyK',
+      type: 'pop',
+  },
+  {
+      src: './audio/Arkadi Dumikyan-Champa tveq.mp3',
+      name: 'Arkadi Dumikyan - Champa tveq',
+      code: 'KeyL',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Aram Asatryan-Masis.mp3',
+      name: 'Aram Asatryan - Masis',
+      code: 'KeyM',
+      type: 'classic',
+  },
+  {
+      src: './audio/miyagi_-_korabli.mp3',
+      name: 'Miyagi - Korabli',
+      code: 'KeyN',
+      type: 'rock',
+  },
+  {
+      src: './audio/Bebe-Cocaine.mp3.crdownload',
+      name: 'Bebe - Cocaine',
+      code: 'KeyO',
+      type: 'pop',
+  },
+  {
+      src: './audio/Arkadi Dumikyan-Angel Moy 2016- 2017.mp3',
+      name: 'Arkadi Dumikyan - Angel Moy',
+      code: 'KeyP',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Aram Asatryan-Surb Sargis.mp3',
+      name: 'Aram Asatryan - Surb Sargis',
+      code: 'KeyQ',
+      type: 'classic',
+  },
+  {
+      src: './audio/miyagi-endshpil_-_moya-dikaya-kassandra.mp3',
+      name: 'Miyagi & Endshpil - Moya Dikaya Kassandra',
+      code: 'KeyR',
+      type: 'rock',
+  },
+  {
+      src: './audio/😈⚡Pride By Booom⚡😈-Khalif - Утопай (Rodesso Remix).mp3',
+      name: 'Khalif - Утопай',
+      code: 'KeyS',
+      type: 'pop',
+  },
+  {
+      src: './audio/Arkadi Dumikyan-44 дня.mp3',
+      name: 'Arkadi Dumikyan - 44 дня',
+      code: 'KeyT',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Арам Асатрян-Es k mernem u k gnam.mp3',
+      name: 'Aram Asatryan - Es k mernem u k gnam',
+      code: 'KeyU',
+      type: 'classic',
+  },
+  {
+      src: './audio/Renkarnacia-Lusabac.mp3',
+      name: 'Renkarnacia - Lusabac',
+      code: 'KeyV',
+      type: 'rock',
+  },
+  {
+      src: './audio/Misha Xramovi-Воспоминание.mp3',
+      name: 'Misha Xramovi - Воспоминание',
+      code: 'KeyW',
+      type: 'pop',
+  },
+  {
+      src: './audio/Arkadi Dumikyan-Brat.mp3',
+      name: 'Arkadi Dumikyan - Moy Brat',
+      code: 'KeyX',
+      type: 'jazz',
+  },
+  {
+      src: './audio/Valer-Dzer Sery.mp3',
+      name: 'Valer - Dzer Sery',
+      code: 'KeyY',
+      type: 'pop',
+  },
+  {
+      src: './audio/Picca-Lift_(PrimeMusic.ru).mp3',
+      name: 'Picca - Lift',
+      code: 'KeyZ',
+      type: 'jazz',
+  },  
+];
+export let dataPlayList = [
+
+];
+
